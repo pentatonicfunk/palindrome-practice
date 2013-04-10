d9dde93b26b11851c553352544b4fa065a6a1a13
A. Akbar Hidayat <keripix@gmail.com>
2013-04-10 12:20:55 +0700

    first trial

---
+++ README.md
@@ -0,0 +1,9 @@
+#Palindrome Detection
+
+This is just a small experiment with python. It detects wether an input is a palindrome or not.
+
+These examples are detected as palindrome:
+
++ madam
++ Glenelg
++ Rise to vote, Sir
---
+++ palindrome.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import re
+
+"""
+A simple exercise on detecting a palindrome.
+"""
+
+
+def reverse(text):
+    return cleanse(text)[::-1]
+
+
+def isPalindrome(text):
+    return cleanse(text).lower() == reverse(text).lower()
+
+
+def cleanse(text):
+    return re.sub(r'[^A-Za-z]+', '', text)
+
+
+if __name__ == '__main__':
+    text = input("Enter text: ")
+
+    if (isPalindrome(text)):
+        print("Yes, it is a palindrome")
+    else:
+        print("Nope, it is not a palindrome")
